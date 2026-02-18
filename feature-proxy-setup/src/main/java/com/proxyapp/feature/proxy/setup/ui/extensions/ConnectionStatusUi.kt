50b1f74225ae35c3c077767b0a154648d89e4d35
soundnoisen <ldmpxre44@mail.ru>
2026-02-18 16:42:59 +0300

    refactor(proxy-setup): refactor ConnectionStatus extensions

--- feature-proxy-setup/src/main/java/com/proxyapp/feature/proxy/setup/ui/extensions/ConnectionStatusUi.kt
+++ feature-proxy-setup/src/main/java/com/proxyapp/feature/proxy/setup/ui/extensions/ConnectionStatusUi.kt
@@ -1,16 +1,20 @@
 package com.proxyapp.feature.proxy.setup.ui.extensions
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.proxyapp.feature.proxy.setup.R
 import com.proxyapp.feature.proxy.setup.domain.ConnectionStatus
 
-fun ConnectionStatus.titleRes(): Int = when (this) {
-    ConnectionStatus.DISCONNECTED -> R.string.label_status_disconnected
-    ConnectionStatus.CONNECTING -> R.string.label_status_connecting
-    ConnectionStatus.CONNECTED -> R.string.label_status_connected
+@Composable
+fun ConnectionStatus.displayTitle(): String = when (this) {
+    ConnectionStatus.DISCONNECTED -> stringResource(R.string.label_status_disconnected)
+    ConnectionStatus.CONNECTING -> stringResource(R.string.label_status_connecting)
+    ConnectionStatus.CONNECTED -> stringResource(R.string.label_status_connected)
 }
 
-fun ConnectionStatus.hintRes(): Int = when (this) {
-    ConnectionStatus.DISCONNECTED -> R.string.label_disconnect_hint
-    ConnectionStatus.CONNECTING -> R.string.label_connecting_hint
-    ConnectionStatus.CONNECTED -> R.string.label_connected_hint
+@Composable
+fun ConnectionStatus.displayHint(): String = when (this) {
+    ConnectionStatus.DISCONNECTED -> stringResource(R.string.label_disconnect_hint)
+    ConnectionStatus.CONNECTING -> stringResource(R.string.label_connecting_hint)
+    ConnectionStatus.CONNECTED -> stringResource(R.string.label_connected_hint)
 }
